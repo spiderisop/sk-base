19b8f3c50721a23ea67c0b23a5e24bed961e13f9
spiderisop <71515940+spiderisop@users.noreply.github.com>
2020-10-17 15:19:33 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Dashboard</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>
+<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/jvectormap/2.0.4/jquery-jvectormap.min.css'><link rel="stylesheet" href="./style.css">
+<link rel="stylesheet" href="./style1.css">
+
+<style>
+body {
+  background-image: url('img/1.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed; 
+  background-size: 100% 100%;
+}
+</style>
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<section>
+	<header>
+		<nav class="rad-navigation">
+			<div class="rad-logo-container rad-nav-min">
+				<a href="#" class="rad-logo">Checker</a>
+				<a href="#" class="rad-toggle-btn pull-right"><i class="fa fa-bars"></i></a>
+			</div>
+			</nav>
+	</header>
+</section>
+<aside>
+	<nav class="rad-sidebar rad-nav-min">
+		<ul>
+			<li>
+				<a href="#" class="inbox">
+					<i class="fa fa-dashboard"><span class="icon-bg rad-bg-success"></span></i>
+					<span class="rad-sidebar-item">Dashboard</span>
+				</a>
+			</li>
+			<li>
+				<a href="cc/index.php">
+					<i class="fa fa-bar-chart-o">
+						<span class="icon-bg rad-bg-danger"></span>
+					</i>
+					<span class="rad-sidebar-item">Live CC-CHECKER</span>
+				</a>
+			</li>
+			<li><a href="cc-gen/index.php" class="snooz"><i class="fa fa-line-chart"><span class="icon-bg rad-bg-primary"></span></i><span class="rad-sidebar-item">CC-Gen</span></a></li>
+			
+		</ul>
+	</nav>
+</aside>
+<main>
+	<section>
+		<div class="rad-body-wrapper rad-nav-min">
+			<div class="container-fluid">
+			  <br>
+			    <br>
+			      <br>
+			        <br>
+<div class="row">
+
+</div>
+<center>
+
+
+ <div class="container">
+ 
+  <p class="subtitle" align="center">Coded By 🕷🕸𝕊ℙ𝕀𝔻𝔼ℝ🕸🕷</p>
+  </div>
+  <div class="scanlines"></div>
+
+</center>
+
+			</div>
+		</div>
+	</section>
+</main>
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js'></script>
+<script src='https://code.jquery.com/ui/1.11.4/jquery-ui.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jQuery-slimScroll/1.3.3/jquery.slimscroll.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.8.0/lodash.min.js'></script>
+<script src='https://jvectormap.com/js/jquery-jvectormap-2.0.5.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jvectormap/2.0.3/jquery-jvectormap.js'></script>
+<script src='https://jvectormap.com/js/jquery-jvectormap-world-mill.js'></script><script  src="./script.js"></script>
+
+</body>
+</html>
